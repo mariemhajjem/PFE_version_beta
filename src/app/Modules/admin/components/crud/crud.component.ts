7c4619e921c0543fd199538d1c161aa28f03d908
jawedihedi <jawedihedi@gmail.com>
2019-03-19 16:36:36 +0100

    admin navbar

--- src/app/Modules/admin/components/crud/crud.component.ts
+++ src/app/Modules/admin/components/crud/crud.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { FormationService } from '../formation.service';
+import { FormationService } from './../services/formation.service';
 
 @Component({
   selector: 'app-crud',
