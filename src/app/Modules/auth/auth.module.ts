2757d77f806e87020e47b1bc237b05d2ec2c00c8
mariemhajjem <mariemhajjem10@gmail.com>
2019-03-24 17:15:22 +0100

    profil

--- src/app/Modules/auth/auth.module.ts
+++ src/app/Modules/auth/auth.module.ts
@@ -6,16 +6,29 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthService } from './auth.service';
 import{ HttpClientModule } from '@angular/common/http';
+import { AuthorizationService } from '../admin/components/services/authorization.service';
+import { JwtModule, JwtModuleOptions } from '@auth0/angular-jwt';
+
+const JWT_Module_Options: JwtModuleOptions = {
+  config: {
+      tokenGetter: function  tokenGetter() {
+        return     localStorage.getItem('token');},
+      whitelistedDomains: ['localhost:4200/admin']
+  }
+};
+
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
     FormsModule,
     HttpClientModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    JwtModule.forRoot(JWT_Module_Options)
   ],
   providers:[
-    AuthService
+    AuthService,
+    AuthorizationService
   ]
 })
 export class AuthModule { }
