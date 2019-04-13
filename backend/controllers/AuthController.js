128e99994cb8d01a739ffaf02d1c9db16695efd5
mariemhajjem <mariemhajjem10@gmail.com>
2019-04-13 12:58:01 +0100

    profile

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -28,22 +28,14 @@ router.post('/register', function(req, res) {
       res.status(200).send({token:token});
     });
   });
+ 
+router.get('/profile',VerifyToken, function(req, res, next) {
 
-  router.get('/me',  VerifyToken,function(req, res, next) {
-    var token = req.headers['x-access-token'];
-    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
-
-    jwt.verify(token,'secret', function(err, decoded) {
-      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-
-      User.findById(decoded.id,
-        { password: 0 }, // projection
-        function (err, user) {
-          if (err) return res.status(500).send("There was a problem finding the user.");
-          if (!user) return res.status(404).send("No user found.");
-           res.status(200).send(user);
-
-      });
+  User.findById(req.userId, { password: 0 }, function (err, user) {
+    if (err) return res.status(500).send("There was a problem finding the user.");
+    if (!user) return res.status(404).send("No user found.");
+    
+    res.status(200).send(user);
   });
 });
 
--- backend/controllers/VerifyToken.js
+++ backend/controllers/VerifyToken.js
@@ -3,10 +3,11 @@ var jwt = require('jsonwebtoken');
 //middleware function:handle all the authorization 
 function verifyToken(req, res, next) {
   if (!req.headers.authorization)
-    return res.status(401).send('Unauthorized.');
+    return res.status(401).send('Unauthorized....');
    let token= req.headers.authorization.split(' ')[1];
 
-  if(req.headers.authorization && req.headers.authorization.split(' ')[0] === 'token'){
+  if(req.headers.authorization && req.headers.authorization.split(' ')[0]==='Bearer'){
+    console.log(req.headers);
     jwt.verify(token,'secret', function(err, decoded) {
     if (err)
     return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
@@ -18,7 +19,7 @@ function verifyToken(req, res, next) {
    }
   else
   { 
-    return res.status(401).send('Unauthorized.');
+    return res.status(401).send('noooo.');
   }
 }
 module.exports = verifyToken;
