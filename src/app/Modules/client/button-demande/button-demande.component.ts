f1d336b2f24d96dcdbd2bca1f4fa38a795b55a61
jawedihedi <jawedihedi@gmail.com>
2019-05-10 22:34:58 +0200

    aa

---
+++ src/app/Modules/client/button-demande/button-demande.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-demande',
+  templateUrl: './button-demande.component.html',
+  styleUrls: ['./button-demande.component.css']
+})
+export class ButtonDemandeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
